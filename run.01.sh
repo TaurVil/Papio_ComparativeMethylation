5ed1da381cb69fc88e5cd776af91fdafc33f4647
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-12-21 15:26:02 -0600

    Update run.01.sh

--- run.01.sh
+++ run.01.sh
@@ -11,3 +11,13 @@
 	# NOTE 2: map to the baboon and the lambda phage genomes separately. Not doing here as I'm just keeping the previously reported conversion rates. 
   sbatch --array=1-44 --mem=28G run02_trimmap.sh 
 
+# Quality control
+	# Not done for panubis1
+	#Bisulfite conversion rate 
+  for f in `ls *methratio.lam.txt`; do $f; grep 'lambda' $f | awk '{ SUM += $8} END { print SUM}'; grep 'lambda' $f | awk '{ SUM += $7} END { print SUM}'; done
+	#Msp1 Digest
+  for k in `ls *panu.sam`; do $k; grep -P '\t(CGG|TGG)' $k | wc -l ; 
+	grep -P '(CCA|CCG)\t' $k | wc -l ; 
+	grep -P '(CGG|TGG)\t' $k | wc -l ; 
+	grep -P '\t(CCA|CCG)' $k | wc -l ;
+  done
