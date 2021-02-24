9352e0ceb42e675e88f87260b175649aaec95354
TaurVil <41305538+TaurVil@users.noreply.github.com>
2021-02-23 19:33:37 -0600

    Update run.03.sh

--- run.03.sh
+++ run.03.sh
@@ -7,15 +7,42 @@
   cd methratio; module load R; for f in `cat ../00_chroms.txt`; do cat ../run.c2.get_counts_part2.R | sed -e s/CHROMNAME/$f/g > $f.get_data.R; sbatch -N 1 --mem=10000 -p all --nice ./$f.get_data.R; done; cd ..
 
 # Merge into one file for counts, mcounts, and info
+  head -1 info_chr1.txt > n44.info.txt
+  for f in `seq 1 20`; do cat counts_table_chr$f.txt >> n44.counts.txt;  
+    sed '1d' info_chr$f.txt | sed 's/"//g' >> n44.info.txt; 
+    cat mcounts_table_chr$f.txt >> n44.mcounts.txt; done
+
+# only keeping the autosomes
 
 # Remove CpG sites where the C or G is variable
+  # Because C and G counts are condensed, we'll need to remove sites where the variant overlaps the CpG site value or the CpG site value plus 1 (disrupting the C or G)
+
   # Variable sites determined using BisSNP genotype calls for these samples, rather than an externally validated file set
   # found in /data/tunglab/tpv/CM/genotype_all/merged.012.pos
   
+  # Remove sites where the methylation data implies polymorphism (reverse G counts are not the same as reverse GA counts)
+  module load R; R
+  options(scipen = 20)
+  library(data.table); to_remove <- NULL; files <- list.files(pattern="methratio.txt")
+  for (i in files) {
+    fread(i) -> data; subset(data, ! data$rev_G_count == data$rev_GA_count) -> d2
+    rbind(d2, to_remove) -> to_remove
+  }
+  to_remove[!duplicated(paste(to_remove$chr, to_remove$pos, sep="_")),] -> to_rem
+  to_rem[,3] <- to_rem[,2] + 1
+  write.table(to_rem[,1:3], "./variable_CT.bed", row.names=F, col.names=F, sep="\t", quote=F)
+  
+  read.delim("/data/tunglab/tpv/CM/genotype_all/merged.012.pos", header=F) -> snps
+  snps[,3] <- snps[,2] + 1; snps[,2] <- snps[,2] - 1
+  write.table(snps[,1:3], "./snps.bed", row.names=F, col.names=F, sep="\t", quote=F)
+  
+  module load bedtools2; cat snps.bed variable_CT.bed > remove.bed; bedtools sort -i remove.bed > tmp.bed; bedtools merge -i tmp.bed > to_exclude.bed; rm tmp.bed
+  sed '1d' n44.info.txt > tmp; bedtools sort -i tmp > tmp2; bedtools subtract -a tmp2 -b to_exclude.bed | cut -f 1-4 > n44.nonvariable.txt; rm tmp; rm tmp2
+
   # Other option is to identify them from known variants. We'll not do this. 
 	# vcftools --gzvcf known_variants.vcf.gz --012 --out known_variants
 	# vcftools --gzvcf known_variants.vcf.gz --remove-filtered-all --mac 2 --max-alleles 2 --minQ 100 --012 --out filtered_variants
 			
-  # Because C and G counts are condensed, we'll need to remove sites where the variant overlaps the CpG site value or the CpG site value plus 1 (disrupting the C or G)
+  
   
   
